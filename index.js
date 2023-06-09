a9440ac333235dc53fef3c6ea2e73de9fb98b121
ArjumanJesmin <arjumanjesmin221186@gmail.com>
2023-06-09 22:59:24 +0600

    use patch and created admin

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 
 // ---------------------------------------
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dkozdag.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -40,24 +40,40 @@ async function run() {
       res.send(result)
     })
 
+
+
     //users collection------------------------
-    app.post('/users', async (req, res) => {
-      const user = req.body;
-      const result = await userCollection.insertOne(user)
+    app.get('/users', async (req, res) => {
+      const result = await userCollection.find().toArray()
       res.send(result)
     })
 
     app.post('/users', async (req, res) => {
       const user = req.body;
-      console.log(user);
       const query = { email: user.email }
-      const existingUser = await userCollection.findOne(query)
+      const existingUser = await userCollection.findOne(query);
 
       if (existingUser) {
-        return res.send({ message: 'User already exists' })
+        return res.send({ message: 'user already exists' })
       }
-      const result = await userCollection.insertOne(user)
-      res.send(result)
+
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+     
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+
     })
 
 
