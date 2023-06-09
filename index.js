b5e1e3582deffd69afedb825755d2f9e05983575
ArjumanJesmin <arjumanjesmin221186@gmail.com>
2023-06-10 01:26:21 +0600

    setup jwt token

--- index.js
+++ index.js
@@ -3,12 +3,31 @@ const cors = require('cors')
 require('dotenv').config()
 
 
+
 const app = express();
 const port = process.env.PORT || 5000
 
 //middle ware
 app.use(cors())
 app.use(express.json())
+
+// jwt collection------------------------
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
+  // bearer token
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+          return res.status(401).send({ error: true, message: 'unauthorized access' })
+      }
+      req.decoded = decoded;
+      next();
+  })
+}
 
 // ---------------------------------------
 
@@ -33,6 +52,13 @@ async function run() {
     const allDataCollection = client.db("Akibuki").collection('allData')
     const classCollection = client.db("Akibuki").collection('classes')
     const userCollection = client.db("Akibuki").collection('users')
+
+ //jwt collection------------------------
+ app.post('/jwt', (req, res) => {
+  const user = req.body;
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '7d' })
+  res.send({ token })
+})
 
     //allData collection------------------------
     app.get('/allData', async (req, res) => {
